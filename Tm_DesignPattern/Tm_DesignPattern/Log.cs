bd4616c2bc931a3bb2b3adeac880d0521a9e83f0
Rehan <rehanpq@gmail.com>
2018-07-09 14:25:03 +0530

    Push Some project

---
+++ Tm_DesignPattern/Tm_DesignPattern/Log.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tm_DesignPattern
+{
+    public sealed class Log
+    {
+        static int countInstance = 0;
+        static object obj = new object();
+        private static Log Instance;
+        public static Log GetInstance
+        {
+            get
+            {
+
+                if (Instance == null)
+                {
+                    Instance = new Log();
+                   
+                }
+                return Instance;
+            }
+        }
+
+        private Log()
+        {
+            countInstance++;
+        }
+
+
+        public void PrintLog()
+        {
+            Console.WriteLine("Printing Singlon Log");
+            Console.WriteLine($"Instance Count: {countInstance}");
+            Console.ReadLine();
+        }
+
+        public class GeneralLog 
+        {
+            Log obj = new Log();
+
+            public GeneralLog()
+            {
+                var obj2 = obj;
+            }
+
+        }
+    }
+}
---
+++ Tm_DesignPattern/Tm_DesignPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tm_DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Log objLog = Log.GetInstance;            
+            objLog.PrintLog();
+
+            var nestedObj = new Log.GeneralLog();
+
+            Log objLog2 = Log.GetInstance;
+            objLog2.PrintLog();
+
+            
+        }
+    }
+}
